46bc7c36654afdf42ef36701a806f42fcdebf55e
Neil Seejoor <nvseejoor@gmail.com>
2018-08-16 22:30:08 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   get '/secret' => 'secrets#show'
   post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destory'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
