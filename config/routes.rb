36d5c9ce796b39eeca54e3c423f10eabb55d5d80
Neil Seejoor <nvseejoor@gmail.com>
2018-08-16 22:26:34 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'application#hello'
   get '/login' => 'sessions#new'
   get '/secret' => 'secrets#show'
   # You can have the root of your site routed with "root"
